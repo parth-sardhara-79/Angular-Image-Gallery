f2d133f4be9f6f1aaf52d9c02402eed955f830d1
parth-sardhara-79 <parthsardhara026@gmail.com>
2019-05-05 13:17:25 +0530

    Image Gallery first commit

---
+++ src/app/image/image.component.ts
@@ -0,0 +1,54 @@
+import { Component, OnInit } from '@angular/core';
+import { Lightbox, LightboxConfig, LightboxEvent, LIGHTBOX_EVENT, IEvent, IAlbum } from 'ngx-lightbox';
+import { Subscription } from 'rxjs';
+
+@Component({
+  selector: 'app-image',
+  templateUrl: './image.component.html',
+  styleUrls: ['./image.component.scss']
+})
+export class ImageComponent implements OnInit {
+
+  public albums: Array<IAlbum>;
+ 
+  private _subscription: Subscription;
+  constructor(
+    private _lightbox: Lightbox,
+    private _lightboxEvent: LightboxEvent,
+    private _lighboxConfig: LightboxConfig
+  ) {
+    this.albums = [];
+    for (let i = 1; i <= 4; i++) {
+      const src = 'assets/Images/cat' + i + '.jpeg';
+      const caption = 'Image ' + i + ' caption here';
+      const thumb = 'assets/Images/cat' + i + '-thumb.jpeg';
+      const album = {
+         src: src,
+         caption: caption,
+         thumb: thumb
+      };
+
+      this.albums.push(album);
+    }
+
+    // set default config
+    this._lighboxConfig.fadeDuration = 1;
+  }
+
+  open(index: number): void {
+    this._subscription = this._lightboxEvent.lightboxEvent$.subscribe((event: IEvent) => this._onReceivedEvent(event));
+
+    // override the default config
+    this._lightbox.open(this.albums, index, { wrapAround: true, showImageNumberLabel: true });
+  }
+
+  private _onReceivedEvent(event: IEvent): void {
+    if (event.id === LIGHTBOX_EVENT.CLOSE) {
+      this._subscription.unsubscribe();
+    }
+  }
+
+  ngOnInit() {
+  }
+
+}
